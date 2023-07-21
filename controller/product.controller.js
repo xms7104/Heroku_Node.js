8c1e8f4ac9be97d2c818f23df7a50d57559c68de
xms7104 <minsan.x@doomeye.com>
2023-07-21 16:12:55 +0800

    remove console.log and adjust idData

--- controller/product.controller.js
+++ controller/product.controller.js
@@ -19,8 +19,8 @@ pool.getConnection((err, conn) => {
 });
 
 const productController = {
-    getAll: async (req, res) => { await pool.query(
-        "SELECT * FROM product",
+    getAll: async (req, res) => { 
+        await pool.query( "SELECT * FROM product",
         (err, result) => {
             if (err) {
             console.log(err);
@@ -29,32 +29,18 @@ const productController = {
             }
         }
         );
-        // try {
-        //     const rows = await pool.query("select * from product");
-        //     res.json({
-        //       dataRows: rows,
-        //     });
-        // } catch (error) {
-        //     console.log("Error in getAll method:", error); // 输出实际的错误信息
-        //     res.json({
-        //         status: "error_getAll",
-        //         error: error // 将错误信息添加到响应中
-        //     });
-        // }
     },
     getById: async (req, res) => {
-        try {
-            const { id } = req.params
-            const [rows, fields] = await pool.query("select * from product where id = ?", [id])
-            res.json({
-                data: rows
-            })
-        } catch (error) {
-            console.log(error)
-            res.json({
-                status: "error"
-            })
+        const { id } = req.params
+        await pool.query( "SELECT * FROM product WHERE id = ?", [id],
+        (err, result) => {
+            if (err) {
+            console.log(err);
+            } else {
+            res.send(result);
+            }
         }
+        )
     },
     create: async (req, res) => {
         try {
